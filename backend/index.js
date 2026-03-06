d5c28237b4b83ce323223143d612429161d790a1
likhithaj04 <likhitha15jayram@gmail.com>
2026-03-06 17:02:25 +0530

    Cors fix

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin:[ "http://localhost:5173",
-  "https://aianalyser.onrender.com"
+  "ai-analyser-beta.vercel.app"
 ]
 
  }));
